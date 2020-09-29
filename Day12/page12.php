d1ad57f69a74d2c6c9ddf7dd5d6821ee9be6eb89
Abhilash Kamble <62751047+AbbyBounty@users.noreply.github.com>
2020-09-29 17:29:29 +0530

    Update page12.php

--- Day12/page12.php
+++ Day12/page12.php
@@ -1,3 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
 <?php
 
 header('Content-Type:text/html');
@@ -8,3 +17,9 @@ print("Reverse String : ".strrev($_GET['number']));
 
 
 ?>
+</body>
+</html>
+
+
+
+
